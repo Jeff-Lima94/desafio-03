5487ddd8f871f003240e0751819d1d1b80ee6af0
Jeff-Lima94 <jeffersonlima10.jls@gmail.com>
2022-07-22 11:59:41 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 10; i++){
+   console.log(i)
+}
+
+let conte =10
+while(conte >= 1){
+   console.log(conte);
+   conte --;
+}
+while (1<= conte){
+   console.log (conte)
+   conte-= 1
+}
+
+for(let impares = 1; impares <=100; impares +=2){
+   console.log (impares)
+}
+
+let pares = 0
+while (pares <= 100){
+   console.log(pares)
+   pares +=2;
+}
